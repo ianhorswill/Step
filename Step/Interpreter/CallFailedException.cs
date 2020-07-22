6b62e2c7945e4c445350620d2b93e3e830a4f1f3
ianhorswill <ian@northwestern.edu>
2020-07-22 00:01:24 -0500

    Fixed warning in CallFailedException

--- Step/Interpreter/CallFailedException.cs
+++ Step/Interpreter/CallFailedException.cs
@@ -6,24 +6,12 @@ namespace Step.Interpreter
     public class CallFailedException : CallException
     {
         /// <summary>
-        /// The task that was called and failed
-        /// </summary>
-        public readonly object Task;
-        /// <summary>
-        /// The arguments to the task
-        /// </summary>
-        public readonly object[] Arguments;
-
-        /// <summary>
         /// Indicates that a call to a Step task that shouldn't be able to fail did fail.
         /// </summary>
         /// <param name="task">Task called</param>
         /// <param name="arguments">Arguments</param>
         public CallFailedException(object task, object[] arguments)
         : base(task, arguments, $"Call failed: {Call.CallSourceText(task, arguments)}")
-        {
-            Task = task;
-            Arguments = arguments;
-        }
+        { }
     }
 }
