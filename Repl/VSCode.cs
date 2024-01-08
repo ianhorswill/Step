8d24b23d079b06500e7a0bf39cfaae6be62fed99
Ian Horswill <ian@northwestern.edu>
2024-01-07 18:56:49 -0600

    Added code to manually locate VS Code binary so we can invoke it directly

--- Repl/VSCode.cs
+++ Repl/VSCode.cs
@@ -8,6 +8,32 @@ namespace Repl
     // ReSharper disable once InconsistentNaming
     public static class VSCode
     {
+        private static readonly string? editorPath;
+
+        static VSCode()
+        {
+            // We need to search for the VS Code installation because, at least on Windows 11, the file called "code" in the search path
+            // isn't the actual executable, it's a shell script.  Somehow if you run "code" from the windows shell, it finds it, but
+            // it doesn't work from Process.Start unless you set UseShell, in which case it's both slower and also pops up a console window.
+            editorPath = "code";
+            foreach (var dir in Environment.GetEnvironmentVariable("PATH")!.Split(';'))
+            {
+                var p = Path.Combine(dir, "Code.exe");
+                if (File.Exists(p))
+                {
+                    editorPath = p;
+                    return;
+                }
+
+                p = Path.Combine(Path.GetDirectoryName(dir)!, "Code.exe");
+                if (File.Exists(p))
+                {
+                    editorPath = p;
+                    return;
+                }
+            }
+        }
+
         /// <summary>
         /// Invoke the editor on the specified folder.
         /// </summary>
@@ -26,7 +52,14 @@ namespace Repl
 
         private static void LaunchEditor(params string[] args)
         {
-            Process.Start(new ProcessStartInfo("code", args) { UseShellExecute = true, CreateNoWindow = true });
+            if (editorPath == null)
+                // VSC not installed
+                return;
+            Process.Start(new ProcessStartInfo(editorPath, args)
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            });
         }
     }
 }
